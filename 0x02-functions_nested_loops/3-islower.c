4092dd4a0863fe69445c0404089c3309404dedca
Silas <workitoutsilas@gmail.com>
2023-07-18 19:58:43 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - checks for alphabetic character
+ * c - character to be checked
+ * return 1 or 0
+ */
+
+
+
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+return (0);
+}
