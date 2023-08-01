81f1947c0226dcc3edcdeb28dc3c3cfe2f448a36
Silas <workitoutsilas@gmail.com>
2023-08-01 16:45:10 -0700

    .

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,13 +11,14 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-int i;
+int r = 0;
+int i = n;
 
-
-for (i = 0; i < n; i++)
-{
-dest[i] = src[i];
+for (; r < i; r++)
+{	
+dest[r] = src[r];
 n--;
+}
 return (dest);
 }
-}
+
