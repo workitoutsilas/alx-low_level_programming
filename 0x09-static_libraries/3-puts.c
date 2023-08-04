de1b91305ff672df397eb4968973e535e4eca2f2
Silas <workitoutsilas@gmail.com>
2023-08-03 20:40:49 -0700

    Tire ei

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+void _puts(char *str);
+#include<stdio.h>
+/**
+ * main - A program that outputs a line with puts function
+ * Return: Always 0 (Success)
+ */
+
+
+
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
